d4b6257bf501589033041880d55da10e2c877c6d
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2015-03-31 22:45:50 +0200

    Initial release, no changes from original

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/SketchBoard/includes/css/sketch-board.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/SketchBoard/includes/css/colorpicker.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/SketchBoard/includes/css/upsell.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/SketchBoard/css/sketch-admin.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/css/ie-style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/css/superfish.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/css/font-awesome.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/css/skt-animation.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/css/font-awesome-ie7.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/css/bootstrap-responsive.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+}
---
+++ readme.txt
@@ -0,0 +1,14 @@
+=== Advertica Light Child ===
+Contributors: Ovidiu-Florin BOGDAN
+Tags: blue, gray, orange, pink, purple, dark, light, two-columns, right-sidebar, left-sidebar, fixed-layout, fluid-layout, responsive-layout, custom-colors, custom-menu, featured-images, full-width-template, post-formats, sticky-post, theme-options, threaded-comments, translation-ready
+Requires at least: 1.0.6
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+A child theme for the Advertica Lite theme, optimized for the Kubuntu project's website
+
+== Changelog ==
+
+= 0.0.1 =
+* Initial release, no changes from original
