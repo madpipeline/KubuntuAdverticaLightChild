b7f5e4fdc71d94b83fa456804bd0c42f42dbbf6e
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2015-04-05 10:34:10 +0200

    fixed main navigation

---
+++ includes/front-header-image-section.php
@@ -0,0 +1,5 @@
+<div class="Skt-header-image">
+	<!-- header image -->
+		<div class="advertica-image-post"><img alt="advertica-default-slider-image" class="ad-slider-image" width="1585"  src="<?php if(sketch_get_option($advertica_shortname.'_frontslider_stype')) { echo sketch_get_option($advertica_shortname.'_frontslider_stype'); } ?>" ></div>
+	<!-- end  header image  -->
+</div>
